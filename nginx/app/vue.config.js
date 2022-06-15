96935ae7219210178521de5de93f21e8573513be
Alexis ANDRIANASY <alexis.andrianasy@ypsi.fr>
2022-06-15 15:26:11 +0200

    mise à jour variable d'environnement

--- nginx/app/vue.config.js
+++ nginx/app/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
+
+process.env.VUE_APP_HTTP_API_URL="https://bobo/api/";
+
 module.exports = defineConfig({
   transpileDependencies: true,
 });
